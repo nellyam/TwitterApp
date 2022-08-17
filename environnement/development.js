2be714c3f4c1bbbd33ef16578e90f3496f455d5a
Nelly <nellynellyam@gmail.com>
2022-08-17 07:21:28 +0200

    fix2

--- environnement/development.js
+++ environnement/development.js
@@ -1,6 +1,6 @@
 const path = require("path")
 module.exports = {
     "dbUrl": "mongodb+srv://nelly:1988@cluster0.v2ixo.mongodb.net/twitter?retryWrites=true&w=majority",
-    "cert": path.join(__dirname, "/ssl/local.crt"),
-    "key": path.join(__dirname, "/ssl/local.key"),
+    "cert": path.join(__dirname, "../ssl/local.crt"),
+    "key": path.join(__dirname, "../ssl/local.key"),
 };
