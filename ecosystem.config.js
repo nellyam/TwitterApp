e52ce54048d0716849bd649da8df8d2b89c7c53d
Nelly <nellynellyam@gmail.com>
2022-08-17 11:57:59 +0200

    fix 5

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,12 +5,6 @@ module.exports = {
     instances: "max",
     watch: true,
     autorestart: true
-  }, 
-  env, {
-    NODE_ENV: "development"
-  },
-  env_production, {
-    NODE_ENV: "production"
-  },
+  }
   ]
 };
