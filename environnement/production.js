78dc0f2c279ac77511f723b2b5a07d79d200a48b
Nelly <nellynellyam@gmail.com>
2022-08-18 17:05:17 +0200

    fix 8

--- environnement/production.js
+++ environnement/production.js
@@ -1,6 +1,6 @@
 const path = require("path")
 module.exports = {
     "dbUrl": "mongodb+srv://nelly:1988@cluster0.v2ixo.mongodb.net/twitter?retryWrites=true&w=majority",
-    "cert": "/etc/letsencrypt/live/nelly-projects.fr/fullchain.pem",
-    "key": "/etc/letsencrypt/live/nelly-projects.fr/privkey.pem",
+    "cert": "/etc/letsencrypt/live/www.nelly-projects.fr/fullchain.pem",
+    "key": "/etc/letsencrypt/live/www.nelly-projects.fr/privkey.pem",
 };
