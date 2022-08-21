b9780433e0b68419d4ab5d576093655cf1259aa9
Nelly <nellynellyam@gmail.com>
2022-08-21 06:50:06 +0200

    fix 25

---
+++ public/javascripts/search-bar.js
@@ -0,0 +1,10 @@
+let menuContainer;
+
+window.addEventListener('click', () => {
+        menuContainer.innerHTML = "";
+    }) 
+
+window.addEventListener('DOMContentLoaded', () => {
+     menuContainer = document.querySelector("#search-menu-container");
+ 
+  })
