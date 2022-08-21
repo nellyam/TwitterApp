86fc5891937e2c426bb0755b07efc8cdbe0314e1
Nelly <nellynellyam@gmail.com>
2022-08-21 12:06:40 +0200

    fix 39

--- environnement/development.js
+++ environnement/development.js
@@ -3,6 +3,6 @@ module.exports = {
     "dbUrl": "mongodb+srv://nelly:1988@cluster0.v2ixo.mongodb.net/twitter?retryWrites=true&w=majority",
     "cert": path.join(__dirname, "../ssl/local.crt"),
     "key": path.join(__dirname, "../ssl/local.key"),
-    portHttp: 3000,
+    portHttp: 80,
     portHttps: 443
 };
