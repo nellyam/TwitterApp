f31bcf4d591e79771b2932829663c4aeea35ec19
Nelly <nellynellyam@gmail.com>
2022-08-21 07:09:48 +0200

    fix 26

--- public/javascripts/search-bar.js
+++ public/javascripts/search-bar.js
@@ -1,3 +1,5 @@
+const e = require("express");
+
 let menuContainer;
 
 window.addEventListener('click', () => {
@@ -7,8 +9,19 @@ window.addEventListener('click', () => {
 window.addEventListener('DOMContentLoaded', () => {
      menuContainer = document.querySelector("#search-menu-container");
      menuContainer.addEventListener("click", (e) => {
-        e.stopPropagation();
-        
-     })
+        e.stopPropagation();        
+     });
+
+     let searchInput = document.querySelector("#search-input");
+     let ref;
+     searchInput.addEventListener("input", (e) => {});
+     const value = e.target.value;
+     if(ref) {
+       clearTimeout(ref);        
+     }
+
+     ref = setTimeout(() => {
+        console.log(value);
+     }, 2000);
  
   })
